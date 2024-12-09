42e9cdb49870398be49173658904b330c4c8945e
Sias Mey <siasmey@gmail.com>
2024-12-09 17:05:23 +0200

    chore: update from Darwin HPE-LFL6NJ14KD at 2024-12-09T17:05:23+02:00

--- scripts/vhm.nix
+++ scripts/vhm.nix
@@ -3,12 +3,12 @@ pkgs.writeShellScriptBin "vhm" ''
   set -e
 
   cd ~/personal/NixHome/trunk
-  ${pkgs.neovim}/bin/nvim .
-  ${pkgs.git}/bin/git add .
-  ${pkgs.git}/bin/git commit -m "chore: update from $(uname -no) at $(date -Iseconds)"
+  nvim .
+  git add .
+  git commit -m "chore: update from $(uname -no) at $(date -Iseconds)"
 
-  ${pkgs.nh}/bin/nh home build ~/personal/NixHome/trunk
-  ${pkgs.git}/bin/git push
+  nh home build ~/personal/NixHome/trunk
+  git push
 
-  ${pkgs.nh}/bin/nh home switch ~/personal/NixHome/trunk
+  nh home switch ~/personal/NixHome/trunk
 ''
