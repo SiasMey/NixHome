d866f4f24c45743da8d5f5c1882d0083963d8cb7
Sias Mey <siasmey@gmail.com>
2024-12-09 11:18:31 +0200

    fix: combine mac and linux hm flakes

--- flake.nix
+++ flake.nix
@@ -9,29 +9,40 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
-    linux = "x86_64-linux";
-  in {
-    defaultPackage.${linux} = home-manager.defaultPackage.${linux};
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    {
+      defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
+      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
 
-    homeConfigurations."meysi@foot1" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.${linux};
+      homeConfigurations."meysi@foot1" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-      modules = [
-        {
-          home.username = "meysi";
-          home.homeDirectory = "/home/meysi";
-        }
-        ./gaming.nix
-        ./home.nix
-        ./unfree.nix
-        ./editor.nix
-        ./hyprland.nix
-      ];
+        modules = [
+          {
+            home.username = "meysi";
+            home.homeDirectory = "/home/meysi";
+          }
+          ./gaming.nix
+          ./home.nix
+          ./unfree.nix
+          ./editor.nix
+          ./hyprland.nix
+        ];
+      };
+
+      homeConfigurations."meysi@HPE-LFL6NJ14KD" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+        modules = [
+          {
+            home.username = "meysi";
+            home.homeDirectory = "/Users/meysi";
+          }
+          ./home.nix
+          ./unfree.nix
+          ./editor.nix
+          ./mac.nix
+        ];
+      };
     };
-  };
 }
