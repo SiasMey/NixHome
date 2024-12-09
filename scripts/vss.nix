7ef7878908ae1cf0ca5b6b843d48892c1815de40
Sias Mey <siasmey@gmail.com>
2024-12-09 17:28:45 +0200

    chore: update vss on Darwin HPE-LFL6NJ14KD at 2024-12-09T17:28:45+02:00

---
+++ scripts/vss.nix
@@ -0,0 +1,19 @@
+{pkgs}:
+pkgs.writeShellScriptBin "vss" ''
+  set -e
+  SCRIPT_NAME=$1
+
+  cd ~/personal/NixHome/trunk/scripts
+  if [ -f !"$SCRIPT_NAME.nix" ]; then
+    echo "{pkgs}:pkgs.writeShellScriptBin \"$SCRIPT_NAME\" \'\'\n\'\'" > "$SCRIPT_NAME.nix"
+  fi
+
+  nvim "$SCRIPT_NAME.nix"
+  git add "$SCRIPT_NAME.nix"
+  git commit -m "chore: update $SCRIPT_NAME on $(uname -no) at $(date -Iseconds)"
+
+  nh home build ~/personal/NixHome/trunk
+  git push
+
+  nh home switch ~/personal/NixHome/trunk
+''
