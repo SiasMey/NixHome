519f46da09d717a52dc8f0e230361de2f22ca8c7
Sias Mey <siasmey@gmail.com>
2024-12-11 08:51:52 +0200

    chore: update vss config \nfrom Darwin HPE-LFL6NJ14KD at 2024-12-11T08:51:51+02:00\n2024-12-11 00:38 : id 100 -> /nix/store/0m8z28g4di9b0jpndlpzxkmf6n3iqphj-home-manager-generation

--- scripts/vss.nix
+++ scripts/vss.nix
@@ -11,7 +11,16 @@ pkgs.writeShellScriptBin "vss" ''
   nvim "$SCRIPT_NAME.nix"
   git add "$SCRIPT_NAME.nix"
   git add ../shell-scripts.nix
-  git commit -m "chore: update $SCRIPT_NAME config \nfrom $(uname -no) at $(date -Iseconds)\n$(home-manager generations | head -n 1)"
+
+  temp_file=$(mktemp)
+  echo "chore: update shell script $SCRIPT_NAME" >> $temp_file
+  echo "" >> $temp_file
+  echo "" >> $temp_file
+  echo "from: $(uname -no)" >> $temp_file
+  echo "$(home-manager generations | head -n 1)" >> $temp_file
+
+  git commit -f $temp_file
+  rm $temp_file
 
   nh home build ~/personal/NixHome/trunk
 
