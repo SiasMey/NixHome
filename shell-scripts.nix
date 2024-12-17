0ec1a718ee489a8daad2af449e65fa43678348d5
Sias Mey <siasmey@gmail.com>
2024-12-17 15:35:53 +0200

    chore: update shell script vm
    from: Darwin HPE-LFL6NJ14KD
    2024-12-13 16:24 : id 121 -> /nix/store/w3xqdwc1vfwmq3za3h6mvihc4wkmhy5w-home-manager-generation

--- shell-scripts.nix
+++ shell-scripts.nix
@@ -4,5 +4,6 @@
     (import ./scripts/vss.nix {inherit pkgs;})
     (import ./scripts/vv.nix {inherit pkgs;})
     (import ./scripts/vt.nix {inherit pkgs;})
+    (import ./scripts/vm.nix {inherit pkgs;})
   ];
 }
