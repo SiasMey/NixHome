5391c8afceadbf316dce151b6243e6755d7f336f
Sias Mey <siasmey@gmail.com>
2024-12-19 16:13:24 +0200

    chore: update neovim config
    from: Darwin HPE-LFL6NJ14KD
    2024-12-19 16:12 : id 154 -> /nix/store/3m0j9k2cbwdk3akd85p831gl17xyyr34-home-manager-generation

--- dotfiles/nix-nvim/lua/plugins/motions.lua
+++ dotfiles/nix-nvim/lua/plugins/motions.lua
@@ -1,30 +1,6 @@
 local function setup_leap()
   local leap = require("leap")
   leap.opts.highlight_unlabeled_phase_one_targets = true
-
-  local augroup = vim.api.nvim_create_augroup("leap_fixes", { clear = true })
-  vim.api.nvim_create_autocmd("User", {
-    group = augroup,
-    pattern = "LeapEnter",
-    callback = function()
-      vim.cmd.hi("Cursor", "blend=100")
-      vim.opt.guicursor:append({ "a:Cursor/lCursor" })
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("User", {
-    group = augroup,
-    pattern = "LeapLeave",
-    callback = function()
-      vim.cmd.hi("Cursor", "blend=0")
-      vim.opt.guicursor:remove({ "a:Cursor/lCursor" })
-    end,
-  })
-
-  vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
-  vim.api.nvim_set_hl(0, "LeapMatch", { fg = "magenta", bold = true, italic = true, nocombine = true })
-  vim.api.nvim_set_hl(0, "LeapLabel", { fg = "red", bold = true, italic = true, nocombine = true })
-
   vim.keymap.set("n", "m", "<Plug>(leap)")
   vim.keymap.set("n", "em", function()
     require("leap.remote").action()
